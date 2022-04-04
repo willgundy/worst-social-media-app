3e0fa98fa548470e186c26e7a571c19420af6f6e
Cody-Wise <cody@sharptackmedia.com>
2022-04-04 14:23:11 -0700

    Setup createprofile and get profiles and supabase tables

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,6 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://afgbmdkvqbvliaergujk.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmZ2JtZGt2cWJ2bGlhZXJndWprIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc2Mzg2NTUsImV4cCI6MTk2MzIxNDY1NX0.VyU9_hrFWQ13GXnm_YwMxhGCqRVI1VMlopV5PCqYqYI'
+;
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -19,8 +20,30 @@ export function redirectIfLoggedIn() {
     }
 }
 
+export async function createProfile(){
+    const response = await client
+        .from('profiles')
+        .insert({})
+        .single();
+
+
+    return response.body;
+}
+
+export async function getProfiles(){
+    const response = await client
+        .from('profiles')
+        .select('*');
+
+    return response.body;
+}
+
+
+
 export async function signupUser(email, password) {
     const response = await client.auth.signUp({ email, password });
+
+    await createProfile();
 
     return response.user;
 }
