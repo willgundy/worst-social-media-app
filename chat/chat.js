6f66841ce2c4ccc16d2ef326b96e9ccd92943a9c
JeffAllison <itsjallison@gmail.com>
2022-04-06 10:38:27 -0700

    created chat window load event listener in chat.js

--- chat/chat.js
+++ chat/chat.js
@@ -13,127 +13,28 @@ import {
 } from '../fetch-utils.js';
 
 const logoutButton = document.getElementById('logout');
-const form = document.querySelector('form');
-const messageContainer = document.querySelector('.messages');
-const addButton = document.querySelector('.addButton');
-const decrementButton = document.querySelector('.decrementButton');
-const karmaHeaderEl = document.querySelector('.karma-class');
-const imageInput = document.querySelector('#profileImage');
-const profileName = document.querySelector('#profileName');
-const profileAvatar = document.querySelector('#profileAvatar');
-
-const params = new URLSearchParams(window.location.search);
-const id = params.get('id');
-
-imageInput.addEventListener('change', async (e) => {
-  const imageFile = e.target.files[0];
-
-  const filePath = `public/${imageFile.name}.jpg`;
-
-  const tablePath = 'https://afgbmdkvqbvliaergujk.supabase.co/storage/v1/object/public/profile-images/' + filePath;
-
-  await imageUpload(filePath, imageFile);
-
-  updateProfileImage(id, tablePath);
-
-  profileAvatar.src = tablePath;
-});
+const theChatsEl = document.querySelector('the-chats');
+const formEl = document.querySelector('form');
 
 checkAuth();
-
-form.addEventListener('submit', async e => {
-  e.preventDefault();
-
-  const data = new FormData(form);
-  const message = data.get('message');
-
-  const profileInfo = await getMyProfile();
-  const senderId = profileInfo.id;
-  const recipientId = id;
-  const messageContent = { sender_id: senderId, recipient_id: recipientId, text: message };
-
-  await createMessage(messageContent);
-
-  form.reset();
-});
-
-window.addEventListener('load', async () => {
-  fetchAndDisplayProfile();
-});
-
-window.addEventListener('load', async () => {
-  await client
-    .from('*')
-    .on('*', payload => {
-      if (payload.eventType === 'INSERT') {
-        renderMessages();
-      }
-    })
-    .subscribe();
-});
-
-function shortDate (date) {
-  var dateFormat = new Date(date);
-
-  return dateFormat.toLocaleString('en-US');
-
-  // (dateFormat.getMonth() + 1) + 
-  // '/' + dateFormat.getDate() + '/' + dateFormat.getFullYear() + '' + dateFormat.getHours() + ':' + dateFormat.getMinutes();
-}
-
-async function fetchAndDisplayProfile (profile) {
-  //karma header on profile
-  renderKarma(profile);
-
-  //create messages for profile
-  renderMessages();
-}
-
-async function renderMessages () {
-  messageContainer.innerHTML = '';
-
-  const displayMessages = await getMessages(id);
-
-  for (let message of displayMessages) {
-    const messageDiv = document.createElement('p');
-    messageDiv.classList.add('messagetext');
-
-
-    const shortDateText = shortDate(message.created_at);
-
-
-    messageDiv.textContent = `${message.text} from ${message.profiles.email} (${message.profiles.karma}) at ${shortDateText} `;
-
-    messageContainer.append(messageDiv);
-  }
-}
-
-
-async function renderKarma (profile) {
-  const { email, karma, avatar } = profile ? profile : await getProfile(id);
-  karmaHeaderEl.textContent = `Karma for ${email} is ${karma}`;
-
-  profileName.textContent = email;
-  profileAvatar.src = avatar;
-}
-
-
-
-
 
 logoutButton.addEventListener('click', () => {
   logout();
 });
 
-decrementButton.addEventListener('click', async () => {
-  const profile = await decrementKarma(id);
 
-  fetchAndDisplayProfile(profile);
+window.addEventListener('load', async () => {
+  await client
+    .from('chats')
+    .on('INSERT', payload => {
+      const chatItemOuterEl = document.createElement('div');
+      const chatSenderEl = document.createElement('p');
+      const chatMessageEl = document.createElement('p');
+
+      chatSenderEl.textContent = payload.new.sender_email;
+      chatMessageEl.textContent = payload.new.text;
+      chatItemOuterEl.append(chatMessageEl, chatSenderEl);
+      theChatsEl.append(chatItemOuterEl);
+    });
+    .subscribe()
 });
-
-addButton.addEventListener('click', async () => {
-  const profile = await incrementKarma(id);
-
-  fetchAndDisplayProfile(profile);
-});
-
